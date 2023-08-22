5bae7e07311c7517a56abc58d91857608be8b74f
martinaemunoz <martinaemunoz@gmail.com>
2023-08-22 17:09:23 -0400

    Adds trending movies

---
+++ movie-challenge-fw/src/App.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    const url = 'https://api.themoviedb.org/3/trending/movie/week?api_key=db946c84f7b0a4d24fbc4e2ec032838e';
+    const res = await fetch(url);
+    const data = await res.json();
+    setMovies(data.results);
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return (
+    <div>
+      <h1 className="main-title">Now Streaming</h1>
+      {movies.map(movie => (
+        <div key={movie.id} className="movie-container">
+          <div className="movie-image-container">
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} className="movie-image"/>
+          <p className="movie-title">{movie.title}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
